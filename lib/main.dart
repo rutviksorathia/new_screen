3c772d7730f04d42fc8f1adaeeb964b243d79630
SorathiaRutvik <sorathiarutvik2000>
2022-05-26 14:31:23 +0530

    commite

--- lib/main.dart
+++ lib/main.dart
@@ -136,7 +136,7 @@ class MyApp extends StatelessWidget {
                       Container(
                         height: 40,
                         width: 100,
-                        margin: EdgeInsets.only(top: 50, left: 60),
+                        margin: EdgeInsets.only(top: 50, left: 50),
                         decoration: BoxDecoration(
                           color: Colors.white,
                           borderRadius: BorderRadius.all(Radius.circular(10)),
@@ -153,6 +153,193 @@ class MyApp extends StatelessWidget {
                       ),
                     ],
                   ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 25),
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        height: 20,
+                        width: 130,
+                        margin: EdgeInsets.only(top: 7, left: 25),
+                        color: Colors.black,
+                        child: Text(
+                          'Portfolio Earning',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(left: 5),
+                      ),
+                      Container(
+                        height: 20,
+                        width: 70,
+                        margin: EdgeInsets.only(top: 7, left: 25),
+                        color: Colors.black,
+                        child: Text(
+                          'Savings',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 15,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 15),
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        height: 0.5,
+                        width: 140,
+                        margin: EdgeInsets.only(left: 25),
+                        decoration: BoxDecoration(
+                          border: Border.all(width: 1.3, color: Colors.white),
+                        ),
+                      ),
+                      Container(
+                        height: 0.5,
+                        width: 245,
+                        margin: EdgeInsets.only(left: 0, right: 10),
+                        decoration: BoxDecoration(
+                          border: Border.all(width: 0.1, color: Colors.white),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        height: 90,
+                        width: 180,
+                        margin: EdgeInsets.only(left: 25, top: 35),
+                        decoration: BoxDecoration(
+                          color: Colors.grey,
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(10),
+                          ),
+                        ),
+                        child: Column(
+                          children: [
+                            Row(
+                              children: [
+                                Container(
+                                  height: 23,
+                                  width: 100,
+                                  margin: EdgeInsets.only(
+                                    top: 19,
+                                    left: 20,
+                                  ),
+                                  color: Colors.grey,
+                                  child: Text(
+                                    '\$ 20.90',
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                                Container(
+                                    height: 30,
+                                    width: 30,
+                                    margin: EdgeInsets.only(left: 25, top: 17),
+                                    decoration: BoxDecoration(
+                                      color: Colors.white,
+                                      borderRadius:
+                                          BorderRadius.all(Radius.circular(60)),
+                                    ),
+                                    child: Icon(
+                                      Icons.arrow_back,
+                                      color: Colors.green,
+                                    )),
+                              ],
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(top: 5),
+                            ),
+                            Container(
+                              height: 23,
+                              width: 150,
+                              margin: EdgeInsets.only(left: 10),
+                              color: Colors.grey,
+                              child: Text(
+                                'Per Day',
+                                style: TextStyle(
+                                    fontSize: 19, color: Colors.white),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                      Container(
+                        height: 90,
+                        width: 180,
+                        margin: EdgeInsets.only(left: 25, top: 35),
+                        decoration: BoxDecoration(
+                          color: Colors.grey,
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(10),
+                          ),
+                        ),
+                        child: Column(
+                          children: [
+                            Row(
+                              children: [
+                                Container(
+                                  height: 23,
+                                  width: 100,
+                                  margin: EdgeInsets.only(
+                                    top: 19,
+                                    left: 20,
+                                  ),
+                                  color: Colors.grey,
+                                  child: Text(
+                                    '\$ 20.90',
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                                Container(
+                                    height: 30,
+                                    width: 30,
+                                    margin: EdgeInsets.only(left: 25, top: 17),
+                                    decoration: BoxDecoration(
+                                      color: Colors.white,
+                                      borderRadius:
+                                          BorderRadius.all(Radius.circular(60)),
+                                    ),
+                                    child: Icon(
+                                      Icons.arrow_back,
+                                      color: Colors.green,
+                                    )),
+                              ],
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(top: 5),
+                            ),
+                            Container(
+                              height: 23,
+                              width: 150,
+                              margin: EdgeInsets.only(left: 10),
+                              color: Colors.grey,
+                              child: Text(
+                                'Per Day',
+                                style: TextStyle(
+                                    fontSize: 19, color: Colors.white),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  )
                 ],
               ),
             ),
